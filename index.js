70d87ece79f16fc9bf39b948064f743454785071
Villie99 <villie17@hotmail.com>
2025-09-23 09:18:33 +0200

    path fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const recipeRouter = require('./api/recipes'); // relative path
 
 const app = express();
 const PORT = process.env.PORT || 3000;
